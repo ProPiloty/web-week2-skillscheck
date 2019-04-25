c99c3aa2c4150078a103e98f06d57e802e07236d
Tyler Ellingford <tyler@Rickys-MacBook-Pro.local>
2019-04-25 10:54:40 -0600

    completed javascript-2

--- javascript-2/student.js
+++ javascript-2/student.js
@@ -6,25 +6,39 @@
 
 // Code Here
 
+const lovesCode = true;
+
 //////////////////STEP 2////////////////////
 //Create a variable called age that is your age as a number
 
 // Code Here
+
+const age = 23;
 
 //////////////////STEP 3////////////////////
 //Create a variable called name that is your name as a string
 
 // Code Here
 
+const name = 'Tyler';
+
 //////////////////STEP 4////////////////////
 //Create a variable called phrase that is initially set as an empty string
 
 // Code Here
 
+const phrase = '';
+
 //////////////////STEP 5////////////////////
 //Create an if statement that checkes to see if the lovesCode variable is equal to true, if it is, set the phrase variable created above to 'I love code', it it's not true set the phrase variable to 'I do not love to code'
 
 // Code Here
+
+if (lovesCode) {
+    phrase = 'I love code';
+} else {
+    phrase = 'I do not love code';
+}
 
 //////////////////Wrap Up////////////////////
 // Refresh the index.html page in your browser. You should now see a poem that uses the variables from above.
